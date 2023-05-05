e72d3a0b3a2cf8dc7e64fd729158a94514265f89
Professor <Professor@SN-331493.edu.fiesc.com.br>
2023-05-05 16:21:39 -0300

    Versão final

--- loja.js
+++ loja.js
@@ -2,12 +2,30 @@ let salva = document.getElementById('salva')
 let calcula = document.getElementById('calcula')
 let res = document.getElementById('res')
 let produto_array = [], qtde_array = []
+let blusa = 35.50, calca = 55.50, bermuda = 45.50
 
 salva.addEventListener('click',()=>{
     let produto = document.getElementById('produto').value
     let qtde = Number(document.getElementById('qtde').value)    
     produto_array.push(produto)
     qtde_array.push(qtde)
+    console.log(produto_array)
+    console.log(qtde_array)
+})
+
+calcula.addEventListener('click',()=>{
+    let total = 0.0
+    for(i=0;i<produto_array.length;i++){
+        if(produto_array[i]="blusa"){
+            total = total + (blusa*qtde_array[i])
+        }else if(produto_array[i]="calca"){
+            total = total + (calca*qtde_array[i])
+        }else if(produto_array[i]="bermuda"){
+            total = total + (bermuda*qtde_array[i])
+        }
+    }
+    res.innerHTML = 'O valor total das compras é R$ ' + total.toFixed(2)
+
 })
 
 
